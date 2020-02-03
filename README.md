487f3a95ffce8a992dfdb8fc181ef2b7fef62560
avery-rock <averyjrock@gmail.com>
2020-02-03 01:42:28 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This project seeks to answer a simple question: if you have more than two eyeballs and close some, but not all, of them, is it blinking or winking? Does the fraction of eyes closes matter? How about symmetry? Duration? 
+
+Do we need another, entirely different word to capture previously unimagined combinations of open and closed eyes? 
+
+This simple code generates .gif animations of black and white grids of cartoon eyeballs that open and close randomly. Their behaviors and rendering can be controlled to produce the desired effect overall. By default, a single .gif file called "eyeballs.gif" will be placed in the current directory. 
+
+Art credit: @Smanfth 
---
+++ blinky.m
@@ -0,0 +1,67 @@
+function blinky(n, resolution, frames, frameRate, blinkRate, closedFraction)
+% Produces and exports a gif with square tiles of cartoony eyes that blink
+% regularly. The size, number, blink rate,
+
+close all; clc
+eyeball_size = resolution/n;
+
+I = rgb2gray(imread("eyes.png"))/255;
+f = zeros(eyeball_size, eyeball_size, 3);
+wide = imresize(I(50:280, 460:710), [eyeball_size, eyeball_size], "method", "nearest");
+middle = imresize(I(70:320, 30:300), [eyeball_size, eyeball_size], "method", "nearest");
+closed = imresize(I(340:560, 500:720), [eyeball_size, eyeball_size], "method", "nearest");
+skip_odds = .5*blinkRate/frameRate
+A = ones(resolution);
+[X, Y] = meshgrid(1:eyeball_size:resolution, 1:eyeball_size:resolution);
+pos = [X(:), Y(:)];
+n = size(pos, 1);
+
+isOpen = true(1, n);
+blinking = false(1, n);
+
+for i = 1:n % for each eyeball
+    A(pos(i, 1):pos(i, 1) + eyeball_size - 1, pos(i, 2):pos(i, 2) + eyeball_size - 1) = wide;
+end
+exportAsGif(A, 1, frameRate)
+for frame = 2:frames
+    for i = 1:n % for each eyeball
+        if blinking(i)
+            isOpen(i) = not(isOpen(i));
+            blinking(i) = false;
+            if isOpen(i)
+                A(pos(i, 1):pos(i, 1) + eyeball_size - 1, pos(i, 2):pos(i, 2) + eyeball_size - 1) = wide;
+            else
+                A(pos(i, 1):pos(i, 1) + eyeball_size - 1, pos(i, 2):pos(i, 2) + eyeball_size - 1) = closed;
+            end
+        elseif rand > skip_odds
+            % do nothing
+        elseif isOpen(i) && rand > (1 - closedFraction)
+            blinking(i) = true;
+            A(pos(i, 1):pos(i, 1) + eyeball_size - 1, pos(i, 2):pos(i, 2) + eyeball_size - 1) = middle;
+        elseif not(isOpen(i)) && rand > closedFraction
+            blinking(i) = true;
+            A(pos(i, 1):pos(i, 1) + eyeball_size - 1, pos(i, 2):pos(i, 2) + eyeball_size - 1) = middle;
+        end
+        
+    end
+    exportAsGif(A, frame, frameRate)
+end
+
+end
+
+function exportAsGif(A, n, frameRate)
+% h = gcf;
+% axis tight manual % this ensures that getframe() returns a consistent size
+filename = 'testAnimated.gif';
+
+% Capture the plot as an image
+% frame = getframe(h);
+im = imresize(A, [500, 500], "method", "nearest");
+[imind,cm] = gray2ind(im,2);
+% Write to the GIF File
+if n == 1
+    imwrite(imind,cm,filename,'gif', 'Loopcount',inf,'DelayTime',1/frameRate);
+else
+    imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',1/frameRate);
+end
+end
